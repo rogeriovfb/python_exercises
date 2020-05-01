428ae40ce1648e0a3daeb398cec638c4f5bbfd85
Rogerio <ballestrin.rogerio@gmail.com>
2020-04-30 21:32:54 -0300

    Otimização do codigo do exercicio 56 da seção 6.

--- secao_6_exercicio_56.py
+++ secao_6_exercicio_56.py
@@ -2,16 +2,14 @@
 Faça um programa que calcule a soma de todos numeros primos abaixo de dois milhões
 """
 
-maximo = 20000
-primos = [2]
-
-for num in range(3, maximo+1):
+maximo = 2000000
+soma = 0
+for num in range(3, maximo+1, 2):
     for i in range(2, num):
         if num % i == 0:
             break
         elif i > num/2:
-            primos.append(num)
+            soma += num
             break
 
-# print(primos)
-print(f'O somátorio de todos numeros primos abaixo de {maximo} é {sum(primos)}.')
+print(f'O somátorio de todos numeros primos abaixo de {maximo} é {soma}.')
