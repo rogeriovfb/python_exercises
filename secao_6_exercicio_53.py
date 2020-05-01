5ddba205da03a8bb6e4cbc5d4737404d843cbf6c
Rogerio <ballestrin.rogerio@gmail.com>
2020-05-01 17:12:12 -0300

    Secao 6 - Exercício 53

---
+++ secao_6_exercicio_53.py
@@ -0,0 +1,12 @@
+"""
+Escreva um programa que leia um numero inteiro positivo n e em seguida imprima n linhas do triangulo de Floyd
+"""
+
+linhas = int(input('Insira o número de linhas do triângulo: '))
+contador = 1
+
+for k in range(1, linhas+1):
+    print()
+    for i in range(1,k+1):
+        print(contador, end = ' ')
+        contador += 1
